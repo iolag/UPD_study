b4dd5cb0018316d89fad54840f1d084c184d5e70
iolag <63402398+iolag@users.noreply.github.com>
2022-10-04 17:18:23 +0200

    new

--- UPD_study/utilities/evaluate.py
+++ UPD_study/utilities/evaluate.py
@@ -25,22 +25,8 @@ def evaluate(config: Namespace, test_loader: DataLoader, val_step: Callable) -> 
     segmentations = []
     benchmark_step = 0
     total_elapsed_time = 0
-    if config.test:
-        contrasts = []
-        mean_intensities = []
-        sizes = []
-        # total_mean_score = []
-        intensities = []
-        norm_intensities = []
-        norm_intensities_r = []
-        norm_intensities_g = []
-        norm_intensities_b = []
-        intensities_r = []
-        intensities_g = []
-        intensities_b = []
-        size_norm = []
-        size_les = []
-        # forward pass the testloader to extract anomaly maps, scores, masks, labels
+
+    # forward pass the testloader to extract anomaly maps, scores, masks, labels
     # forward pass the testloader to extract anomaly maps, scores, masks, labels
     for input, mask in tqdm(test_loader, desc="Test set", disable=config.speed_benchmark):
 
@@ -56,22 +42,6 @@ def evaluate(config: Namespace, test_loader: DataLoader, val_step: Callable) -> 
 
         label = torch.where(mask.sum(dim=(1, 2, 3)) > 0, 1, 0)
         labels.append(label)
-        if config.test:
-            norm_intensity = [inp[torch.logical_and(msk == 0, inp > 0)].flatten(
-            ) for inp, msk in zip(input[:, 0].unsqueeze(1), mask)]
-            norm_intensities.append(torch.cat(norm_intensity))
-            intensity = [inp[msk != 0].flatten() for inp, msk in zip(input, mask)]
-            intensities.append(torch.cat(intensity))
-
-            mean_intensity = torch.tensor([inp[msk != 0].mean() for inp, msk in zip(input, mask)])
-            mean_intensities.append(mean_intensity)
-            # Relative area of lesion
-            lesion_area = torch.count_nonzero(mask, dim=(1, 2, 3))
-            size_les.append(lesion_area)
-            brain_area = torch.count_nonzero(torch.stack(
-                [inp > inp.min() for inp in input]), dim=(1, 2, 3))
-            size_norm.append(brain_area)
-            sizes.append(lesion_area / brain_area)
 
         if config.speed_benchmark:
             benchmark_step += 1
@@ -85,29 +55,7 @@ def evaluate(config: Namespace, test_loader: DataLoader, val_step: Callable) -> 
                       total_elapsed_time / (benchmark_step - 3),
                       "fps: ", 160 / total_elapsed_time)
                 return
-    if config.test:
-        sizes = torch.cat(sizes)
-        print(torch.cat(size_les).sum())
-        print(torch.cat(size_norm).sum())
-        print(torch.count_nonzero(torch.cat(labels)), len(torch.cat(labels)))
-        intensities = torch.cat(intensities)
-        norm_intensities = torch.cat(norm_intensities)
-        mean_intensities = torch.cat(mean_intensities)
-        torch.save(
-            mean_intensities,
-            f'/u/home/lagi/thesis/UAD_study/histogram stuff/statistics/mean_intensities_{config.sequence}_{config.brats_t1}_{config.method}_new.pt')
 
-        torch.save(
-            norm_intensities,
-            f'/u/home/lagi/thesis/UAD_study/histogram stuff/statistics/norm_intensities_{config.sequence}_{config.brats_t1}_{config.method}_new.pt')
-
-        torch.save(
-            intensities,
-            f'/u/home/lagi/thesis/UAD_study/histogram stuff/statistics/intensities_{config.sequence}_{config.brats_t1}_{config.method}_new.pt')
-        torch.save(
-            sizes,
-            f'/u/home/lagi/thesis/UAD_study/histogram stuff/statistics/sizes_{config.sequence}_{config.brats_t1}_{config.method}_new.pt')
-        exit(0)
     # calculate metrics like AP, AUROC, on pixel and/or image level
     metrics(config, anomaly_maps, segmentations, anomaly_scores, labels)
 
@@ -127,5 +75,4 @@ def evaluate(config: Namespace, test_loader: DataLoader, val_step: Callable) -> 
     # if recon based method, len(x)==3 because val_step returns reconstructions.
     # if thats the case log the reconstructions
     if len(output) == 3:
-
         log({'anom_val/reconstructions': output[2]}, config)
