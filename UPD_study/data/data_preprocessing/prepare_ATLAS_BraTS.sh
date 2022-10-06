f190836187a0d3b720fbcf4403d27c7e4b49b8a7
iolag <63402398+iolag@users.noreply.github.com>
2022-10-06 09:11:32 +0200

    new

--- UPD_study/data/data_preprocessing/prepare_ATLAS_BraTS.sh
+++ UPD_study/data/data_preprocessing/prepare_ATLAS_BraTS.sh
@@ -3,7 +3,7 @@
 # conda activate lagi
 
 #Download ATLAS
-get https://fcon_1000.projects.nitrc.org/indi/retro/ATLAS/releases/R2.0/ATLAS_R2.0_encrypted.tar.gz
+wget https://fcon_1000.projects.nitrc.org/indi/retro/ATLAS/releases/R2.0/ATLAS_R2.0_encrypted.tar.gz
 openssl aes-256-cbc -md sha256 -d -a -in ATLAS_R2.0_encrypted.tar.gz -out ATLAS_R2.0.tar.gz
 tar -xvzf ATLAS_R2.0.tar.gz -C UPD_study/data/datasets/MRI/ATLAS
 rm ATLAS_R2.0.tar.gz
