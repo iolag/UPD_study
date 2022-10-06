86a7e8031aa49f38d94dcdd06ce4378f2e1d41d3
iolag <63402398+iolag@users.noreply.github.com>
2022-10-06 10:20:38 +0200

    new

--- UPD_study/data/data_preprocessing/prepare_BraTS.sh
+++ UPD_study/data/data_preprocessing/prepare_BraTS.sh
@@ -2,14 +2,6 @@
 # source /opt/anaconda3/etc/profile.d/conda.sh
 # conda activate lagi
 
-#Download ATLAS
-wget https://fcon_1000.projects.nitrc.org/indi/retro/ATLAS/releases/R2.0/ATLAS_R2.0_encrypted.tar.gz
-openssl aes-256-cbc -md sha256 -d -a -in ATLAS_R2.0_encrypted.tar.gz -out ATLAS_R2.0.tar.gz
-tar -xvzf ATLAS_R2.0.tar.gz -C UPD_study/data/datasets/MRI/ATLAS
-rm ATLAS_R2.0.tar.gz
-rm ATLAS_R2.0_encrypted.tar.gz
-rm -r UPD_study/data/datasets/MRI/ATLAS/ATLAS_2/Testing
-
 #Download BraTS 
 kaggle datasets download -d awsaf49/brats20-dataset-training-validation
 unzip brats20-dataset-training-validation.zip -d UPD_study/data/datasets/MRI
@@ -21,4 +13,3 @@ mv UPD_study/data/datasets/MRI/BraTS/MICCAI_BraTS2020_TrainingData/BraTS20_Train
 
 # Register and skullstrip
 python UPD_study/data/data_preprocessing/prepare_data.py --dataset BraTS
-python UPD_study/data/data_preprocessing/prepare_data.py --dataset ATLAS
